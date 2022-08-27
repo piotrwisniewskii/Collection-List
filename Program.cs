d2dbf28db76e812d782c8bb37f4c586cbbaa1163
Piotr Wisniewski <piowisni@gmail.com>
2022-08-27 13:36:12 +0200

    IndexAdded

--- Program.cs
+++ Program.cs
@@ -12,3 +12,29 @@ foreach (var name in names)
 {
     Console.WriteLine($"Hello {name.ToUpper()}!");
 }
+
+Console.WriteLine($"My name is {names[0]}");
+Console.WriteLine($"I've added {names[2]} and {names[3]} to the list");
+Console.WriteLine($"The list has {names.Count} people in it");
+
+
+
+var index = names.IndexOf("Felipe");
+if (index == -1)
+{
+    Console.WriteLine($"When an item is not found, IndexOf return {index}");
+}
+else
+{
+    Console.WriteLine($"The name {names[index]} is at index {index}");
+}
+
+index = names.IndexOf("Not Found");
+if (index == -1)
+{
+    Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+}
+else
+{
+    Console.WriteLine($"The name {names[index]} is at index {index}");
+}
