4564a0aa5bf80327bae88b3d13c1ba31829f83de
Piotr Wisniewski <piowisni@gmail.com>
2022-08-27 13:25:33 +0200

    Added foreach

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var names = new List<string> { "Piotr", "Ana", "Felipe"};
+foreach (var name in names)
+{
+    Console.WriteLine($"Hello {name.ToUpper()}!");
+}
+
+Console.WriteLine();
+names.Add("Maria");
+names.Add("Bill");
+names.Remove("Ana");
+foreach (var name in names)
+{
+    Console.WriteLine($"Hello {name.ToUpper()}!");
+}
